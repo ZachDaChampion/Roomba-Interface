8cf15772e79990491af0310fca8094a12b2d192d
ZachDaChampion <zachchampion79@gmail.com>
2020-12-26 20:39:52 -0500

    Main plots graphs for position and velocity, honestly just ignore this file

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 import time
+import pandas as pd
 import matplotlib.pyplot as plt
 from roomba_interface import Roomba, RoombaMode
 
-roomba = Roomba('COM4')
-# roomba.reset()
+roomba = Roomba('COM4', _vel_filter_strength=.1)
 roomba.setMode(RoombaMode.SAFE)
-roomba.beginSensorLoop()
+roomba.beginUpdateLoop()
 roomba.setDigitsASCII('help')
 
-print(roomba.sensorData['enc-left'], roomba.sensorData['enc-right'])
 roomba.driveVel(200, 200)
 time.sleep(1)
 roomba.driveVel(0, 0)
 time.sleep(1)
-print(roomba.sensorData['enc-left'], roomba.sensorData['enc-right'])
-roomba.driveVel(-100, -100)
-time.sleep(2)
+roomba.driveVel(-200, -200)
+time.sleep(1)
 roomba.driveVel(0, 0)
 time.sleep(1)
-print(roomba.sensorData['enc-left'], roomba.sensorData['enc-right'])
 
 
 roomba.close()
-plt.plot(roomba.encoderData)
+data = pd.DataFrame(roomba.sensorData.history)
+plt.figure(1)
+plt.subplot(211)
+plt.plot(data['enc-left'])
+plt.plot(data['enc-right'])
+plt.subplot(212)
+plt.plot(data['vel-left'])
+plt.plot(data['vel-right'])
 plt.show()
